e17352235defcc0eb0ecd30f4cd2aae2ba7cf3cf
Kacper <stefaniuk880@gmail.com>
2026-01-19 14:32:15 +0100

    feat(Authorize): Add authorize to admin controller

--- src/main/java/com/secure/spring_security/security/SecurityConfig.java
+++ src/main/java/com/secure/spring_security/security/SecurityConfig.java
@@ -1,30 +1,26 @@
 package com.secure.spring_security.security;
 
-import com.secure.spring_security.model.AppRole;
-import com.secure.spring_security.model.Role;
-import com.secure.spring_security.model.User;
-import com.secure.spring_security.repositories.RoleRepository;
-import com.secure.spring_security.repositories.UserRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
-import java.time.LocalDate;
-
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
 public class SecurityConfig {
 
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
+        http.authorizeHttpRequests((requests) -> requests
+                .requestMatchers("/api/admin/**").hasRole("ADMIN")
+                .anyRequest().authenticated());
         http.csrf(AbstractHttpConfigurer::disable);
         //http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
